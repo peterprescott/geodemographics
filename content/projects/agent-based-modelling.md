68f29e9c377bbf18477a736214330cf12c4ef1e6
peterprescott <peter@prescott.ws>
2019-09-22 02:08:07 +0100

    add js abm 101 to agent-based project page

--- content/projects/agent-based-modelling.md
+++ content/projects/agent-based-modelling.md
@@ -7,3 +7,11 @@ draft: false
 This project is the result of working through the practicals for [our first core module](https://www.geog.leeds.ac.uk/courses/computing/study/core-python-phd/), hosted by Leeds University.
 
 You can find the source code in [my Github repository](https://github.com/peterprescott/agent-based-modelling).
+
+I then tried [the same sort of thing in JavaScript](https://github.com/peterprescott/js-agent-modelling), so that the graphical interface could be easily displayed in a web browser:
+<center>
+		<svg id="backdrop" width=500 height=500 style="border:1px solid black"></svg>
+</center>
+<script src="https://d3js.org/d3.v3.min.js"></script>
+<script src="/projects/abm101.js"></script>
+
---
+++ static/js/abm101.js
@@ -0,0 +1,149 @@
+// script.js
+
+console.log('connected!')
+
+
+const backdrop = document.getElementById('backdrop')
+let total_agents = 0
+let agents = []
+let num_of_agents = 3
+let step_size = 1
+let neighbourhood = 5
+
+
+class Agent {
+  constructor(x, y) {
+    // set initial values
+    this.id = total_agents;
+    total_agents ++;
+    this.x = x;
+    this.y = y;
+    this.step_size = step_size;
+    this.store = 0
+    agents.push(this)
+    }
+
+    move(){
+        if (Math.random() < 0.5) {this.x = (((this.x + this.step_size) % 500) + 500) % 500} else {this.x = (((this.x - this.step_size) % 500) + 500) % 500}
+        if (Math.random() < 0.5) {this.y = (((this.y + this.step_size) % 500) + 500) % 500} else {this.y = (((this.y - this.step_size) % 500) + 500) % 500}    
+        this.interact()
+    }
+
+	eat(){
+		if (env.matrix[Math.round(this.x/100)][Math.round(this.y/100)] > 0){
+			console.log('eating');
+			env.matrix[Math.round(this.x/100)][Math.round(this.y/100)] = env.matrix[Math.round(this.x/100)][Math.round(this.y/100)] - .1
+		}
+		
+	}
+
+    interact(){
+        for (let j=0 ; j < agents.length ; j++){ 
+        if (distance_between(this.id, j) < neighbourhood ){
+            if(this.id==j){}
+            else{console.log('do something interactive')}
+        }
+    }
+    }
+
+}
+
+class Environment {
+    constructor(width, height) {
+        // randomly generate environment rectangle
+        this.matrix = []
+        this.width = width
+        this.height = height
+
+		for (let x = 0; x < this.width; x++){
+			let row = []
+			for (let y = 0; y < this.height; y++){
+				row.push(Math.round(Math.random()*155+100))
+			}
+		this.matrix.push(row)
+		}
+
+
+    }
+
+
+}
+
+
+function distance_between(m,n){
+    let distance_squared = (agents[m].x-agents[n].x)**2 + (agents[m].y-agents[n].y)**2
+    let distance = distance_squared**(1/2)
+    return distance
+}
+
+function draw(agents){
+    // erase Agents
+    var circle = d3.selectAll('circle').remove()
+    
+    // draw newly-positioned Agents
+    for (let i=0; i < agents.length; i++){
+        let agent = agents[i];
+        backdrop.innerHTML += '<circle fill="white" cx="' 
+                                + agent.x 
+                                + '" cy="' 
+                                + agent.y 
+                                + '" r="10" id="circle' 
+                                + agent.id 
+                                + '"></circle>';
+        }
+    }
+
+var env = new Environment(6,6)
+function paint(backdrop, env = Environment){
+	// erase pixels
+	var pixels = backdrop.innerHTML = ''
+	
+	// draw newly positioned pixels
+	for (let x = 0; x < env.width; x++){
+			for (let y = 0; y < env.height; y++){
+					//draw env.matrix[x][y] pixel
+					let posX = x*100-50
+					let posY = y*100-50
+					backdrop.innerHTML += '<rect fill="rgb(0,' + env.matrix[x][y] + ',0)" x="' + posX + '" y="' + posY + '" width="100" height="100" />'
+			}
+
+		}
+
+
+
+}
+
+
+// generate agents
+function generate(num_of_agents){
+	    agents = []
+    for (i = 0; i < num_of_agents; i++){
+	    new Agent(Math.random()*500, Math.random()*500);
+    }
+    paint(backdrop, env)
+    draw(agents)
+}
+
+function iterate(){
+        for (i = 0; i < agents.length; i++){
+            agents[i].move()
+            agents[i].eat()
+        }
+		paint(backdrop, env)
+        draw(agents)
+
+ }
+
+function more(){
+	console.log('still with you')
+	getNumber = Number(document.getElementById('agentnumber').value)
+	console.log('got the number '+getNumber)
+	generate(getNumber)
+
+}
+
+
+generate(num_of_agents)
+
+var refreshIntervalId =  setInterval(iterate, 10)
+
